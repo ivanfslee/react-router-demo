1ecd376806ba156c575768388e87c7ac8c8b45ee
ivanfslee <ivanfslee@gmail.com>
2019-12-16 18:45:42 -0800

    Added react-router-dom and Router/Route/Link components

--- src/App.js
+++ src/App.js
@@ -1,25 +1,24 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
+
+
+const Home = () => <h1>Home</h1>
+const About = () => <h1>About</h1>
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <h1>header</h1>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+      </div>
+      <Route exact path="/" component={Home} />
+      <Route path="/about"  component={About} />
+      <h1>footer</h1>
+    </Router>
   );
 }
 
