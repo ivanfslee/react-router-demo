b3355093ed04e1bf3e6c15be3c87c1136795f31d
ivanfslee <ivanfslee@gmail.com>
2019-12-16 19:16:15 -0800

    Added basic routing to home/help/sign up/log in pages

--- src/App.js
+++ src/App.js
@@ -2,22 +2,23 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
-
+import NavBar from './NavBar/NavBar';
 
 const Home = () => <h1>Home</h1>
-const About = () => <h1>About</h1>
+const Host = () => <h1>Host</h1>
+const Help = () => <h1>Help</h1>
+const Login = () => <h1>Login</h1>
+const Signup = () => <h1>Signed up</h1>
 
 function App() {
   return (
     <Router>
-      <h1>header</h1>
-      <div>
-        <Link to="/">Home</Link>
-        <Link to="/about">About</Link>
-      </div>
-      <Route exact path="/" component={Home} />
-      <Route path="/about"  component={About} />
-      <h1>footer</h1>
+      <NavBar />
+      <Route exact path="/" component={Home}/>
+      <Route exact path="/host" component={Host}/>
+      <Route exact path="/help" component={Help}/>
+      <Route exact path="/login" component={Login}/>
+      <Route exact path="/signup" component={Signup}/>
     </Router>
   );
 }
